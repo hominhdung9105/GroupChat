06dc48bd74a0f459d8f258b006c21ec6b35e0c0c
hominhdung9105 <hominhdung9105@gmail.com>
2026-05-18 19:58:41 +0700

    change popup emoji to emoji picker

--- GroupChat_Client/Views/ChatWindow.xaml.cs
+++ GroupChat_Client/Views/ChatWindow.xaml.cs
@@ -12,5 +12,12 @@ namespace GroupChat_Client.Views
 
             DataContext = new ChatViewModel(client, username, serverIp);
         }
+        private void EmojiPicker_Picked(object sender, Emoji.Wpf.EmojiPickedEventArgs e)
+        {
+            if (DataContext is ChatViewModel viewModel)
+            {
+                viewModel.MessageText += e.Emoji;
+            }
+        }
     }
 }
